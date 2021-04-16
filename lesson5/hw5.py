64357eb99fc09f23abc0de30a987bec7636c9bd7
UlkaBo <ul.bozhko@gmail.com>
2021-04-16 23:11:51 +0300

    Update hw5.py
    add translate ъь

--- lesson5/hw5.py
+++ lesson5/hw5.py
@@ -2,13 +2,22 @@ import sys
 import re
 
 
-# А-Я  Ґ Є І Ї  а-я  ґ є і ї
+# create list ords of kirilic symbols
+#  А-Я  Ґ Є І Ї  а-я  ґ є і ї
 ords_kir = list(range(1040, 1072)) + [1168, 1028, 1030, 1031] + \
     list(range(1072, 1104)) + [1169, 1108, 1110, 1111]
+
+# create list translates latine
 lat_up = 'A B V G D E Zh Z Y Y K L M N O P R S T U F Kh Ts Ch Sh Shch _ Y _ E Yu Ya H Ye I Yi '
 lat_low = lat_up.lower()
 lat = (lat_up + lat_low).split()
+
+# create dictionary from two lists
 d_trunslit = dict(zip(ords_kir, lat))
+
+# replace 'ъь' на ''
+for i in [1066, 1068, 1098, 1100]:
+    d_trunslit[i] = ''
 
 
 def noramlize(text: str):
