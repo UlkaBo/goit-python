6f6cafe3c43694c5c38a93d7a12d31c5e4115070
Ulyana <ul.bozhko@gmail.com>
2021-06-05 16:00:58 +0300

    deleted unnecessary

--- 9modal_yield.py
+++
@@ -1,7 +0,0 @@
-def a(y):
-    print(6)
-    #yield y+1
-
-f=  a(7)
-print(f)
-print(next(f))
--- write_to_file.py
+++
@@ -1,3 +0,0 @@
-with open('save.bin', 'wb') as h :
-    h.write('asdывывы5\n44\n3'.encode())
-    print('asdывывы5\n44\n3'.encode())
